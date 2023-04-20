4cca01d8fd158ac05f83f2d6a928e1167bfe1307
Matt Fowler <mbfowler@gmail.com>
2023-04-20 10:59:46 -0400

    Connect data to the game board

--- main.js
+++ main.js
@@ -12,6 +12,10 @@ var gameSelectionView = document.querySelector('.game-selection');
 var classicGameBoardView = document.querySelector('.classic-game-board');
 var difficultGameBoardView = document.querySelector('.difficult-game-board');
 var gameOption = document.querySelectorAll('.game-option');
+var gameBoards = document.querySelectorAll('.game-board');
+var fighterAreas = document.querySelectorAll('.fighters');
+var humanWins = document.getElementById('humanWins');
+var computerWins = document.getElementById('computerWins');
 
 gameOption.forEach((option) => {
     option.addEventListener('click', function(e) {
@@ -54,17 +58,56 @@ function createPlayer(playerType, playerToken) {
 }
 
 function playGame(chosenFighter) {
-    var computerFighter = getRandomFighter();
-    if (chosenFighter === computerFighter) {
-        return ` emoji It's a draw! emoji`
+    var computerFighter = selectFighter().cloneNode(true);
+    revealFighters(chosenFighter, computerFighter);
+    if (chosenFighter.id === computerFighter.id) {
+        return `emoji It's a draw! emoji`;
     }
     
     var winner = getWinner(chosenFighter, computerFighter)
-    return `emoji ${winner} won this round! emoji`
+    return `emoji ${winner} won this round! emoji`;
+}
+
+function selectFighter(event) {
+    if (!event && game.gameType === 'classic') {
+        return Array.from(fighterAreas[0].children)[getRandomIndex(fighterAreas[0].children)];
+    } else if (!event && game.gameType === 'difficult') {
+        return Array.from(fighterAreas[1].children)[getRandomIndex(fighterAreas[1].children)];
+    } else if (event.target.nodeName === 'IMG') {
+        chosenFighter = event.target.cloneNode(true);
+    }
+}
+
+function revealFighters(chosenFighter, computerFighter) {
+    fighterAreas.forEach((area) => {
+        area.classList.toggle('hidden');
+    })
+
+    var revealArea = document.createElement('div');
+    revealArea.classList.add('hidden');
+    revealArea.append(chosenFighter, computerFighter);
+    
+    var thisGameBoard;
+    if (game.gameType = 'classic') {
+        thisGameBoard = gameBoards[0];
+        gameBoards[0].appendChild(revealArea);
+    } else {
+        thisGameBoard = gameBoards[1];
+        gameBoards[1].appendChild(revealArea);
+    }
+    revealArea.classList.remove('hidden');
+    setTimeout(() => {
+        thisGameBoard.removeChild(revealArea);
+        fighterAreas.forEach((div) => {
+            div.classList.toggle('hidden');
+        })
+    }, 5000);
 }
 
 function getWinner(chosenFighter, computerFighter) {
-    if (rules[chosenFighter].includes(computerFighter)) {
+    if (rules[chosenFighter.id].includes(computerFighter.id)) {
+        incrementWins(game.players[0]);
+        humanWins.innerText = `Wins: ${game.players[0].wins}`;
         return game.players[0].playerType;
     } else {
         return game.players[1].playerType;
